f2bed275d92b31ad66e47abeeaf867de7a6e37ba
Ronald Twitty <61925469+brandon-twitty@users.noreply.github.com>
2021-08-01 14:40:20 -0500

    fixed seller

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ setInterval(function(){
         })
         sma5Promise(global.bitstampData.symbol, '1m').then(sma5 =>{
             console.log(global.bitstampData.symbol,sma5 ,'sma 5 and close' ,global.bitstampData.close)
-            global.bitstampSellData.sell = global.bitstampData.close < sma5
+            global.bitstampSellData.sell = global.bitstampData.fiveAboveTheNine === false
             console.log(global.bitstampData.symbol,'sma 5 and close sell signal at sma 5=' , global.bitstampSellData.sell)
             if(global.bitstampData.sell === true){
                 getAssetBalance(a).then(amount =>{
